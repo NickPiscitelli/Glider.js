4fdfdcd6051ffba7ca51401154d265c2b8bf7ffe
Picky <picky@Pickys-MBP.fios-router.home>
2018-10-16 07:29:31 -0400

    Adding Events

--- glider.js
+++ glider.js
@@ -10,20 +10,17 @@
           var _ = this;
 
           _.ele = element
-          _.dots = [];
-          // ease-in out expo
-          _.easing = function(x, t, b, c, d){
-            if (t==0) return b;
-            if (t==d) return b+c;
-            if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
-            return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
-          };
 
           _.opt = _.extend({
-              prevArrow: true,
-              nextArrow: true,
               slidesToShow: 1,
-              dots: true,
+              duration: 125,
+              easing: function(x, t, b, c, d){
+                // ease-in out expo
+                if (!t) return b;
+                if (t==d) return b+c;
+                if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+                return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+              },
           }, settings);
 
           _.originalOptions =  _.opt;
@@ -31,15 +28,15 @@
           _.instanceUid = instanceUid++;
 
           _.init();
-
-
+          
+          window.addEventListener('resize', _.init.bind(_, true));
       }
 
       return Glider;
 
   }());
 
-  Glider.prototype.init = function() {
+  Glider.prototype.init = function(refresh) {
 
       var _ = this;
 
@@ -54,7 +51,7 @@
       _.containerHeight = 0;
 
       var width = 0, height = 0;
-      _.opt = _.getSettingsBreakpoint()
+      _.opt = _.getSettingsBreakpoint();
       _.itemWidth = Math.floor(_.containerWidth / _.opt.slidesToShow);
 
       [].forEach.call(_.slides, function(__){
@@ -74,16 +71,18 @@
       _.trackWidth = width;
 
       _.bindArrows();
-      _.showArrows();
       _.buildDots();
-      _.ele.addEventListener('scroll', _.showArrows.bind(_))
+      _.updateControls();
+      _.ele.addEventListener('scroll', _.updateControls.bind(_))
+
+      _.ele.dispatchEvent(new Event('glider-'+(refresh ? 'refresh' : 'loaded', {bubbles: !!_.opt.propagateEvents}));
   };
 
   Glider.prototype.buildDots = function(){
     var _ = this
 
     var fragment = document.createDocumentFragment(),
-      ul = document.createElement('ul');
+       ul = document.createElement('ul');
     ul.className = 'glider-dots';
     [].forEach.call(_.slides, function(ele, i){
       var li = document.createElement('li');
@@ -91,13 +90,13 @@
       li.className = i ? '' : 'glider-active';
       li.addEventListener('click',_.scrollItem.bind(_, i))
       ul.appendChild(li);
-      _.dots.push(li);
     });
+    _.dots = ul;
     fragment.appendChild(ul);
     _.ele.parentNode.insertBefore(fragment, _.ele.nextSibling);
 
   }
-
+ 
   Glider.prototype.bindArrows = function(){
     var _ = this;
     ['prev','next'].forEach(function(direction){
@@ -110,7 +109,7 @@
     });
   }
 
-  Glider.prototype.showArrows = function(){
+  Glider.prototype.updateControls = function(){
     var _ = this
     _.prevArrow.classList.toggle('glider-hide', _.ele.scrollLeft <= 0)
     _.nextArrow.classList.toggle('glider-hide', _.ele.scrollLeft + _.ele.offsetWidth >=  _.trackWidth)
@@ -121,8 +120,8 @@
     }
     
     _.activeSlide = Math.floor(_.ele.scrollLeft / _.itemWidth) + 1;
-    console.log(_.activeSlide)
-    _.dots.forEach(function(dot,index){
+
+    if (_.dots) [].forEach.call(_.dots.children,function(dot,index){
       dot.classList.toggle('glider-active', _.activeSlide === index + 1)
     })
   }
@@ -136,16 +135,15 @@
     if (typeof slide === 'string')  slide = slide === 'prev' ? (_.activeSlide - 2) : (_.activeSlide);
     slide = Math.max(Math.min(slide, _.slides.length), 0)
 
-    _.scrollTo(_.itemWidth * slide, 250)
-    _.showArrows();
+    _.scrollTo(_.itemWidth * slide, _.opt.duration * Math.abs(_.activeSlide - slide + 1));
+    _.updateControls();
     return false;
   }
-
 
   Glider.prototype.getSettingsBreakpoint = function(){
       var _ = this, resp = _.opt.responsive;
       if (resp){
-        resp.forEach(function(v){
+        [].forEach.call(resp,function(v){
           if (window.innerWidth > v.breakpoint){
             return _.extend(_.originalOptions, v);
           }
@@ -160,14 +158,14 @@
       start = new Date().getTime(),
       animate = function(){
         var now = (new Date().getTime() - start);
-        _.ele.scrollLeft = (_.ele.scrollLeft + (scrollTarget - _.ele.scrollLeft) * _.easing(0, now, 0, 1, scrollDuration));
+        _.ele.scrollLeft = (_.ele.scrollLeft + (scrollTarget - _.ele.scrollLeft) * _.opt.easing(0, now, 0, 1, scrollDuration));
         if(now < scrollDuration){
           window.requestAnimationFrame(animate);
         }
       };
 
     window.requestAnimationFrame(animate);
-  };
+  }
 
   Glider.prototype.extend = function(source, properties) {
       for(var property in properties) {
@@ -176,6 +174,21 @@
           }
       }
       return source;
-  };
+  }
+
+  Glider.prototype.remove = function(ele){
+    ele.parentElement.removeChild(ele)
+  }
+
+  Glider.prototype.destroy = function(){
+    var _ = this
+    [_.ele,_.track,_.slides].forEach(function(_){
+      _.style.width = 'auto';
+      _.style.height = 'auto';
+    });
+    _.remove(_.prevArrow)
+    _.remove(_.nextArrow)
+    _.remove(_.dots)
+  }
 
 }());
