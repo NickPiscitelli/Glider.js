a144f478c8cd3562fb50bf5e484efbf628383809
Nick Piscitelli <mail@nickpiscitelli.com>
2018-10-15 20:53:07 -0400

    Dots and Stuff

--- glider.js
+++ glider.js
@@ -1,10 +1,8 @@
-(function($) {
+if (window.HTMLCollection && !HTMLCollection.prototype.forEach) HTMLCollection.prototype.forEach = Array.prototype.forEach;
+(function() {
   'use strict';
-  var Glider = window.Glider || {};
-
-  if (window.HTMLCollection && !HTMLCollection.prototype.forEach) HTMLCollection.prototype.forEach = Array.prototype.forEach;
-    
-  Glider = (function() {
+  
+  var Glider = window.Glider = (function() {
 
       var instanceUid = 0;
       
@@ -13,24 +11,32 @@
           var _ = this;
 
           _.ele = element
+          _.dots = [];
+          // ease-in out expo
+          _.easing = function(x, t, b, c, d){
+            if (t==0) return b;
+            if (t==d) return b+c;
+            if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+            return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+          };
 
           _.opt = _.extend({
               prevArrow: true,
               nextArrow: true,
+              dots: true,
               slidesToShow: 1
           }, settings);
 
           _.origOpt =  _.opt;
-
+          _.activeSlide = 0;
           _.instanceUid = instanceUid++;
 
           _.init();
 
 
       }
-    
-    window.Glider = Glider;
-    return Glider;
+
+      return Glider;
 
   }());
 
@@ -56,7 +62,6 @@
           __.style.height = 'auto';
           __.style.width = _.itemWidth + 'px';
           width += _.itemWidth;
-        console.log(_.itemWidth)
           height = Math.max(__.offsetHeight, height);
       });
       if (_.opt.equalHeight){
@@ -67,51 +72,74 @@
 
       _.track.style.width = width + 'px';
       _.trackWidth = width;
-console.log('TW: '+width)
+
       _.bindArrows();
       _.showArrows();
-
+      _.buildDots();
       _.ele.addEventListener('scroll', Glider.prototype.showArrows.bind(_))
   };
+
+  Glider.prototype.buildDots = function(){
+    var _ = this
+
+    var fragment = document.createDocumentFragment(),
+      ul = document.createElement('ul');
+    ul.className = 'glider-dots';
+    _.slides.forEach(function(ele, i){
+      var li = document.createElement('li');
+      li.setAttribute('data-index', i);
+      li.className = i ? '' : 'glider-active';
+      ul.appendChild(li);
+      _.dots.push(li);
+    });
+    fragment.appendChild(ul);
+    _.ele.parentNode.insertBefore(fragment, _.ele.nextSibling);
+
+  }
 
   Glider.prototype.bindArrows = function(){
     var _ = this;
     ['prev','next'].forEach(function(direction){
       var arrow = _.opt[direction+'Arrow'];
-      console.log(arrow)
       if (arrow){
         if (typeof arrow === 'string')  arrow = document.querySelector(arrow);
-        arrow.addEventListener('click', Glider.prototype.scrollItem.bind(_))
+        _[direction+'Arrow'] = arrow;
+        arrow.addEventListener('click', Glider.prototype.scrollItem.bind(_, direction))
       }
     });
   }
 
   Glider.prototype.showArrows = function(){
     var _ = this
-    _.ele.classList.toggle('show-arrows', _.ele.offsetWidth < _.trackWidth);
-    _.ele.classList.toggle('hide-prev', _.ele.scrollLeft <= 0)
-    _.ele.classList.toggle(
-      'hide-next',
-      _.ele.scrollLeft + _.offsetWidth >=  _.trackWidth
-    )
+    _.prevArrow.classList.toggle('glider-hide', _.ele.scrollLeft <= 0)
+    _.nextArrow.classList.toggle('glider-hide', _.ele.scrollLeft + _.ele.offsetWidth >=  _.trackWidth)
+    if (_.ele.offsetWidth >= _.trackWidth){
+      [ _.prevArrow, _.nextArrow ].forEach(function(ele){
+        ele.classList.add('glider-hide');
+      });
+    }
+    
+    _.activeSlide = Math.floor(_.ele.scrollLeft / _.itemWidth);
+    _.dots.forEach(function(dot,index){
+      dot.classList.toggle('glider-active', _.activeSlide === index)
+    })
   }
 
-  Glider.prototype.scrollItem = function(e){
+  Glider.prototype.scrollItem = function(direction, e){
+
     if(e)   e.preventDefault();
 
     var _ = this
 
     var num = 1, scrollLeft = _.ele.scrollLeft;
-    console.log(_.ele)
-    console.log(_.ele.scrollLeft)
+
     if (_.ele.scrollLeft >= _.itemWidth)    num = Math.ceil(scrollLeft / _.itemWidth);
     if (_.ele.scrollLeft === _.itemWidth * num) num++;
     scrollLeft = _.itemWidth * num;
+    console.log(direction)
+    if (direction === 'prev')    scrollLeft -= (_.itemWidth * 2);
 
-    if (_.ele.classList.contains('prev'))    scrollLeft -= (_.itemWidth * 2);
-console.log('scroll top')
-    //_.ele.scrollLeft = scrollLeft;
-    _.scrollTo(.5, scrollLeft)
+    _.scrollTo(250, scrollLeft)
     _.showArrows();
     return false;
   }
@@ -127,18 +155,20 @@ console.log('scroll top')
       }
       return _.opt;
   }
-  
+
   Glider.prototype.scrollTo = function(scrollDuration, scrollTarget) {
-   
     var _ = this
-    var scrollStep = _.ele.scrollLeft / (scrollDuration / 15),
-    scrollInterval = setInterval(function(){
-        if ( _.ele.scrollLeft !== scrollTarget ) {
-            _.ele.scrollLeft = scrollStep;
-        }
-        else clearInterval(scrollInterval); 
-    },15);
-  }
+    var start = new Date().getTime(), animate = function(){
+      var now = (new Date().getTime() - start);
+      _.ele.scrollLeft = (_.ele.scrollLeft + (scrollTarget - _.ele.scrollLeft) * _.easing(0, now, 0, 1, scrollDuration));
+      console.log([now, scrollDuration])
+      if(now < scrollDuration){
+        window.requestAnimationFrame(animate);
+      }
+    };
+    window.requestAnimationFrame(animate);
+  };
+
   Glider.prototype.extend = function(source, properties) {
       for(var property in properties) {
           if(properties.hasOwnProperty(property)) {
@@ -149,12 +179,3 @@ console.log('scroll top')
   };
 
 }());
-
-
-window.addEventListener('load',function(){
-new Glider(document.getElementById('glider'),{
-  slidesToShow: 1,
-  prevArrow: '.prev',
-  nextArrow: '.next'
-})
-});
