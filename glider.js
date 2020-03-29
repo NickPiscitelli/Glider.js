5d96dfb2ef36d0816d7114cb0afa998535644273
Michael Guild <michaelguild13@gmail.com>
2020-03-29 11:38:57 -0400

    calculate with ints vs strigns

--- glider.js
+++ glider.js
@@ -346,8 +346,8 @@
           slide = _.slide
         }
 
-        if (backwards) slide -= _.opt.slidesToScroll
-        else slide += _.opt.slidesToScroll
+        if (backwards) slide -= Number(_.opt.slidesToScroll)
+        else slide += Number(_.opt.slidesToScroll)
 
         if (_.opt.rewind) {
           var scrollLeft = _.ele.scrollLeft
