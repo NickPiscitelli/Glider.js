7130df7042de671e0c176ae2eae0bf3273d12106
Ngoc Tu Nguyen <nguyenngoct2112@gmail.com>
2021-01-14 23:35:50 +0100

    feat(glider.js): add aria-disabled attribute
    better usability with screen reader

--- glider.js
+++ glider.js
@@ -261,6 +261,11 @@
           'disabled',
           _.ele.scrollLeft <= 0 || disableArrows
         )
+        if (_.arrows.prev.classList.contains('disabled')) {
+          _.arrows.prev.setAttribute('aria-disabled', true);
+        } else {
+          _.arrows.prev.setAttribute('aria-disabled', false);
+        }
       }
       if (_.arrows.next) {
         _.arrows.next.classList.toggle(
@@ -268,6 +273,11 @@
           Math.ceil(_.ele.scrollLeft + _.containerWidth) >=
             Math.floor(_.trackWidth) || disableArrows
         )
+        if (_.arrows.next.classList.contains('disabled')) {
+          _.arrows.next.setAttribute('aria-disabled', true);
+        } else {
+          _.arrows.next.setAttribute('aria-disabled', false);
+        }
       }
     }
 
