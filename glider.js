2533cdd1a39d1507c20b6df870bab54c2614690c
Martin Badin <martin.badin@gmail.com>
2021-12-09 20:29:35 +0100

    Shorthand to setting aria-hidden

--- glider.js
+++ glider.js
@@ -262,9 +262,11 @@
           'disabled',
           _.ele.scrollLeft <= 0 || disableArrows
         )
-        _.arrows.prev.classList.contains('disabled')
-          ? _.arrows.prev.setAttribute('aria-disabled', true)
-          : _.arrows.prev.setAttribute('aria-disabled', false)
+
+        _.arrows.prev.setAttribute(
+          'aria-disabled',
+          _.arrows.prev.classList.contains('disabled')
+        )
       }
       if (_.arrows.next) {
         _.arrows.next.classList.toggle(
@@ -272,9 +274,11 @@
           Math.ceil(_.ele.scrollLeft + _.containerWidth) >=
             Math.floor(_.trackWidth) || disableArrows
         )
-        _.arrows.next.classList.contains('disabled')
-          ? _.arrows.next.setAttribute('aria-disabled', true)
-          : _.arrows.next.setAttribute('aria-disabled', false)
+
+        _.arrows.next.setAttribute(
+          'aria-disabled',
+          _.arrows.next.classList.contains('disabled')
+        )
       }
     }
 
